813f101fd976c83694646219de0c5feedc00bc07
bi zi <thebizi15@gmail.com>
2022-04-29 10:37:37 +0300

    change commit name

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 80;
+app.use(express.static(__dirname + '/'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/src/index.html'));
+});
+
+app.listen(PORT, () => {
+  
+});
