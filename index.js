9b6d215adf13ef202baa4cb71836f6ab2862c265
bi zi <thebizi15@gmail.com>
2022-09-01 16:42:43 +0300

    Update for Netlify

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 80;
 app.use(express.static(__dirname + '/'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/src/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
